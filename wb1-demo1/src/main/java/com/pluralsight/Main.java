bcebb0f6fc9d0de4cca536eaf56a738b4288d870
ccalbino <christian.albino89@gmail.com>
2025-04-14 10:54:29 -0400

    added strings plus and expression to produce hello world

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String word1 ="Hello"; //semi// -colon is like period
+
+        String word2 = "World"; //"world" is expressed as a literal value
+
+        String greeting; //define greeting but no literal value
+
+        greeting = word1 + " " + word2 + "!";
+        //we assign greeting an expression. word1 value + space + word2 value + !
+        //concatenation is operators using PEMDAS signs
+
+
+
+        System.out.println(greeting);
     }
 }
