3805c0d273a878eba7041a498ec67fb3b0c016f3
ccalbino <christian.albino89@gmail.com>
2025-04-14 13:49:28 -0400

    Added question and number to solutions

--- MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -5,16 +5,19 @@ public class MathApp {
     public static void main(String[] args) {
 
         // Question 1:
+        System.out.println("1. Determine whose salary is greater using Math.max() and store the answer in highest Salary");
         double bobSalary = 70_000;
         double garySalary = 85_000;
 
-        double highestSalary = Math.max(bobSalary, garySalary);
+        double highestSalary = Math.min(bobSalary, garySalary);
 
         System.out.println("The answer is" + " " + highestSalary);
 
 
 
         // Question 2:
+        System.out.println("2. Find and display the smallest of two variables named carPrice and truckPrice");
+
 
         double carPrice = 53_000;
         double truckPrice = 73_000;
@@ -24,6 +27,8 @@ public class MathApp {
 
 
         //Question 3
+        System.out.println("3. Find and display the area of a circle whose radius is 7.25 ");
+
 
         double radius = 7.25;
         double area = Math.pow(radius, 2) * Math.PI;
@@ -33,6 +38,8 @@ public class MathApp {
 
 
         //Question 4
+        System.out.println("4. Find and display the square root a variable after it is set to 5.0");
+
 
         double rootVariable = 5;
 
@@ -40,18 +47,22 @@ public class MathApp {
 
 
         //Question 5
+        System.out.println("5. Find and display the distance between the points (5, 10) and (85, 50)");
+
 
         double x1 =5;
         double y1 =10;
         double x2 =85;
         double y2 =50;
-        double distanceFormula = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        double distanceFormula = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 
         System.out.println("The distance between points are " + distanceFormula);
 
 
 
         //Question 6
+        System.out.println("6. Find and display the absolute (positive) value of a variable after it is set to -3.8");
+
 
         double var = -3.8;
 
@@ -60,6 +71,9 @@ public class MathApp {
 
         //Question 7
         //double randomNum;
+        System.out.println("7. Find and display a random number between 0 and 1");
+
+
 
         System.out.println("The random number is " + Math.random());
 // declare variables here
