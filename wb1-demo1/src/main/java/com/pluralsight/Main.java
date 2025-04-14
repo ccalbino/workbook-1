37ec76ac941c7cebffd17dac4e8562af8d81c0c1
ccalbino <christian.albino89@gmail.com>
2025-04-14 11:52:18 -0400

    finish first lecture

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -16,7 +16,39 @@ public class Main {
         //concatenation is operators using PEMDAS signs
 
 
+        float taxRate = 0.15f;
 
-        System.out.println(greeting);
+        long aLargeNumber = 222222L;
+
+        //
+
+        int aint;
+
+        long blong;
+
+        aint =7;
+
+        blong = aint;
+
+        //
+
+        long aLong;
+        int bint;
+
+        aLong = 777777l;
+        bint = (int) aLong;
+        //This is type Casting. We tell the "long" we want it to read as an int
+
+
+        double degrees = 90.0;
+
+        double answer = Math.sin(degrees);
+
+
+
+
+
+
+        System.out.println(answer);
     }
 }
