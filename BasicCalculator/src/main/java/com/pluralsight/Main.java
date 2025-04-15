357c23f84f50db02c12e9384078ac699e46e73c3
ccalbino <christian.albino89@gmail.com>
2025-04-15 10:16:14 -0400

    added comments on code for clearity

--- BasicCalculator/src/main/java/com/pluralsight/Main.java
+++ BasicCalculator/src/main/java/com/pluralsight/Main.java
@@ -9,14 +9,17 @@ public class Main {
 
         //Input First number
         System.out.println("Enter the first number");
-        double firstNumber;
+        double firstNumber;//leavs a CRLF next on the stream
         firstNumber = scanner.nextDouble();
+        // also written as double firstNumber = scanner.nextDouble();
 
         //Input Second number
         System.out.println("Enter the second number");
-        double secondNumber;
+        double secondNumber; //leaves a CRLF next on the stream
         secondNumber = scanner.nextDouble();
+        // also written as double secondNumber = scanner.nextDouble();
         scanner.nextLine();
+        //Do this to remove CRLF on stream to run string below
 
         //Choose Possible calculations
         System.out.println("Possible calculations:\n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide\nPlease select an option:");
@@ -25,7 +28,7 @@ public class Main {
 
 
         double product = firstNumber * secondNumber;
-        System.out.printf("The produt of %f and %f is %f", firstNumber ,secondNumber, product);
+        System.out.printf("The product of %f and %f is %f", firstNumber ,secondNumber, product);
 
 
     }
