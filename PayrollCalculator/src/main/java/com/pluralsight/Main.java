075875d5df24b8e9ee0bc82afdfabc7e85ad41a1
ccalbino <christian.albino89@gmail.com>
2025-04-15 14:33:19 -0400

    added if statement to payroll code

--- PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -11,35 +11,76 @@ public class Main {
         //Ask name
 
         System.out.println("Hello! What is your name?");
-        String name;
-        name = scanner.nextLine();
+        String name = scanner.nextLine();
 
         //Ask hours worked
 
         System.out.println("Hi " + name + ", how many hours did you work this week?");
-        double hoursWorked;
-        hoursWorked = scanner.nextDouble();
+        float hoursWorked;
+        hoursWorked = scanner.nextFloat();
 
+
+        // Ask Payrate
         System.out.println("Enter your pay rate");
         double payRate;
         payRate = scanner.nextDouble();
 
+        //If statment for OT
 
-        double product = hoursWorked * payRate;
-        System.out.printf( name + " worked %f hours and made %f per hour to gross %f per week", hoursWorked ,payRate, product);
 
-        //If statement
 
+        double grossPay; //= hoursWorked * payRate;
+
+        if (hoursWorked <= 40){
+            grossPay = hoursWorked * payRate;
+        }
+        else{
+
+            float overtimeHours = hoursWorked - 40;
+            double basePay = 40 * payRate;
+            double overtimeRate = payRate * 1.5;
+            double overtimePay = overtimeHours * overtimeRate;
+
+            grossPay = basePay + overtimePay;
+        }
+
+
+
+        System.out.printf( name + " worked %f hours and made %f per hour to gross %f per week", hoursWorked ,payRate, grossPay);
+
+        //If statement for ot
+
+
+
+
+
+        /*
         double totalPay;
+        double payRate;
 
         if (hoursWorked > 40) {
-            double normalPay = 40 * hoursWorked;
+            double payRate = 40 * hoursWorked;
             double overTime = (hoursWorked - 40) + (payRate * 1.5);
 
             totalPay = normalPay + overTime;
-        } else {
-            totalPay = hoursWorked * payRate;
-        }
+            System.out.printf( name + " worked %f hours and made %f per hour to gross %f per week", hoursWorked ,payRate, totalPay);
+
+
+
+
+            if (hoursWorked > 40) {
+                double otRate = (hoursWorked - 40) * (payRate * 1.5);
+                System.out.printf( name + " worked %f hours and made %f per hour to gross %f per week", hoursWorked ,payRate, otRate);
+
+            }
+
+
+
+//
+//        } else {
+//           double totalPay = double hoursWorked * double payRate; */
+
+    // }
 
 
 
